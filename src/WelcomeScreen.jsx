a094d94aa02630407956bad8286d2e4e6b653e85
Ashley Majer <Majerash@gmail.com>
2025-09-09 12:25:49 -0400

    little style

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -7,7 +7,6 @@ export default function WelcomeScreen() {
     <div className="welcome">
       <p>Welcome to Whack-A-Mole!</p>
       <p> Click the mole and gain points.</p>
-      <p> Let out your aggression! Get Points!</p>
       <button onClick={startGame}>Play</button>
       <p>
         <strong>High Score: {highScore}</strong>
